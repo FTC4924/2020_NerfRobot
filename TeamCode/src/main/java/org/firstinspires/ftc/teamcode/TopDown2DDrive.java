40f3d40480d48053e63b4a8b7f055368c98a5993
FTC4924 <nrrobotics@gmail.com>
2020-09-07 21:10:44 -0400

    Tested, had a good bit of drift. Added gyro sensor so that when you push the joystick in a direction it always goes in that direction, worked still with a good bit of drift. Tried to add turn functionality based off of arc turn but there was no difference in the right and left power.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TopDown2DDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TopDown2DDrive.java
@@ -1,13 +1,22 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
 @TeleOp(name="TopDown2DDrive")
 public class TopDown2DDrive extends OpMode {
+
+    BNO055IMU imu;
+    Orientation angles;
 
     private DcMotor middleWheelMotor;
     private DcMotor rightMotor;
@@ -17,6 +26,7 @@ public class TopDown2DDrive extends OpMode {
 
     private double gamepad1LeftStickX;
     private double gamepad1LeftStickY;
+    private double gamepad1RightStickX;
 
     private double angle;
     private double radius;
@@ -29,20 +39,33 @@ public class TopDown2DDrive extends OpMode {
         leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
         leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        powers = new double[2];
+        powers = new double[3];
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(parameters);
 
     }
 
     @Override
     public void loop() {
 
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
         gamepad1LeftStickX = gamepad1.left_stick_x;
         gamepad1LeftStickY = gamepad1.left_stick_y;
+        gamepad1RightStickX = gamepad1.right_stick_x/2;
 
         radius = xyToRadius(gamepad1LeftStickX, gamepad1LeftStickY);
 
         angle = xyToAngle(gamepad1LeftStickX, gamepad1LeftStickY);
-        angle = angleReducer(angle);
+        angle = angleReducer(angle, angles.firstAngle);
 
         powers = angleToBasePower(angle);
 
@@ -50,6 +73,42 @@ public class TopDown2DDrive extends OpMode {
 
             powers[i] *= radius;
 
+        }
+
+        if(gamepad1RightStickX > 0.05){
+            if(powers[2] > 1 - gamepad1RightStickX) {
+
+                powers[2] -= powers[1] + gamepad1RightStickX - 1 + gamepad1RightStickX;
+                powers[1] = 1;
+
+            } else if(powers[2] < -1 + gamepad1RightStickX) {
+
+                powers[1] -= powers[2] - gamepad1RightStickX + 1 - gamepad1RightStickX;
+                powers[2] = -1;
+
+            } else {
+
+                powers[1] += gamepad1RightStickX;
+                powers[2] -= gamepad1RightStickX;
+
+            }
+        } else if(gamepad1RightStickX < -0.05) {
+            if (powers[2] > 1 + gamepad1RightStickX) {
+
+                powers[1] -= powers[2] - gamepad1RightStickX - 1 - gamepad1RightStickX;
+                powers[2] = 1;
+
+            } else if (powers[2] < -1 - gamepad1RightStickX) {
+
+                powers[2] -= powers[1] + gamepad1RightStickX + 1 + gamepad1RightStickX;
+                powers[1] = -1;
+
+            } else {
+
+                powers[2] -= gamepad1RightStickX;
+                powers[1] += gamepad1RightStickX;
+
+            }
         }
 
         middleWheelMotor.setPower(powers[0]);
@@ -70,9 +129,11 @@ public class TopDown2DDrive extends OpMode {
 
     }
 
-    private double angleReducer(double radians) {
+    private double angleReducer(double radians, double currentAngle) {
 
         double degrees = Math.toDegrees(radians);
+
+        degrees -= currentAngle;
 
         degrees = degrees % 360;
         if(degrees < -180) {
@@ -95,7 +156,7 @@ public class TopDown2DDrive extends OpMode {
         double x;
         double y;
 
-        double[] powers = new double[2];
+        double[] powers = new double[3];
 
         degrees = Math.toDegrees(radians);
 
@@ -123,6 +184,7 @@ public class TopDown2DDrive extends OpMode {
 
         powers[0] = x;
         powers[1] = y;
+        powers[2] = y;
 
         return powers;
 
